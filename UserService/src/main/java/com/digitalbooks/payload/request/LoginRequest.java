4d6b4f8af56c5b6bf360db916399f9fdb4b563f5
mdaamirhussain070 <56185311+mdaamirhussain070@users.noreply.github.com>
2022-12-11 12:03:48 +0530

    Login function working fine

--- UserService/src/main/java/com/digitalbooks/payload/request/LoginRequest.java
+++ UserService/src/main/java/com/digitalbooks/payload/request/LoginRequest.java
@@ -12,6 +12,17 @@ public class LoginRequest {
 	@NotBlank
 	private String email;
 
+	
+	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getUsername() {
 		return username;
 	}
