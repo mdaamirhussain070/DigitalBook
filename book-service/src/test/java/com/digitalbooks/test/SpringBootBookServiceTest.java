da5a2d246775dfe0b2c8f427cfa7140c678f82ac
mdaamirhussain070 <56185311+mdaamirhussain070@users.noreply.github.com>
2022-12-19 18:48:37 +0530

    change in test class file

--- book-service/src/test/java/com/digitalbooks/test/SpringBootBookServiceTest.java
+++ book-service/src/test/java/com/digitalbooks/test/SpringBootBookServiceTest.java
@@ -1,69 +1,12 @@
 package com.digitalbooks.test;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
 
-//@RunWith(SpringRunner.class)
-
-//@SpringBootTest(webEnvironment = WebEnvironment.MOCK, classes = { SpringBootBookServiceTest.class })
 public class SpringBootBookServiceTest {
 
-//	private MockMvc mockMvc;
-//
-//	@Autowired
-//	private WebApplicationContext webApplicationContext;
-//
-//	@MockBean
-//	private BookService bookService;
-//	@MockBean
-//	private BookRepository bookRepository;
-//
-//	@Before
-//	public void setUp() {
-//		this.mockMvc = webAppContextSetup(webApplicationContext).build();
-//	}
-//
-//	@Test
-//	public void create_Book_Test() throws Exception {
-//
-//		Book book = new Book(11, "Hulk", 110, 2, "comic", 50, "marvel", LocalDateTime.now(), LocalDateTime.now(), true,
-//				"Hulk is comming soon");
-//
-//	//	when(bookService.createBook(book)).thenReturn(book);
-//		ObjectMapper objectMapper = new ObjectMapper();
-//		String object = objectMapper.writeValueAsString(book);
-//
-//		mockMvc.perform(post("/digitalbooks/author/11/books").contentType(MediaType.APPLICATION_JSON).content(object)
-//				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
-//				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-//				.andExpect(jsonPath("$.id").value("11"))
-//
-//		;
-//	}
-//
-//	@Test
-//	public void getAll_Book_Test() throws Exception {
-//
-//		Book book1 = new Book(11, "Hulk", 110, 2, "comic", 50, "marvel", LocalDateTime.now(), LocalDateTime.now(), true,
-//				"Hulk is comming soon");
-//		Book book2 = new Book(12, "Hulk", 111, 2, "comic", 50, "marvel", LocalDateTime.now(), LocalDateTime.now(), true,
-//				"Hulk is comming soon");
-//		List<Book> list=Arrays.asList(book1,book2);
-//		
-//		when(bookRepository.findAll()).thenReturn(list);
-////		ObjectMapper objectMapper = new ObjectMapper();
-////		String object = objectMapper.writeValueAsString(list);
-//
-//		mockMvc.perform(get("/digitalbooks/")
-//			//	.contentType(MediaType.APPLICATION_JSON).content(object)
-//				.accept(MediaType.APPLICATION_JSON))
-//				.andExpect(status().isOk())
-//			//	.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-//		
-//
-//		;
-//	}
+	@Test
+	public void contextLoads() {
+		
+	}
 
 }
